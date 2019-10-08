8c300e5dcf368175e8e744932d752f152eede4fe
tentone <tentone@outlook.com>
2019-10-08 21:57:51 +0100

    Derp

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,49 @@ pub type DepthFormat = gfx::format::DepthStencil;
 
 const DERP: f32 = 1e1;
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+const WHITE: [f32; 3] = [1.0, 1.0, 1.0];
+
+const SQUARE: [Vertex; 3] = [
+    Vertex { pos: [0.5, -0.5], color: WHITE },
+    Vertex { pos: [-0.5, -0.5], color: WHITE },
+    Vertex { pos: [-0.5, 0.5], color: WHITE }
+];
+
+gfx_defines! {
+    vertex Vertex {
+        pos: [f32; 2] = "a_Pos",
+        color: [f32; 3] = "a_Color",
+    }
+
+    pipeline pipe {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        out: gfx::RenderTarget<ColorFormat> = "Target0",
+    }
+}	
 
 pub fn main()
 {
+
 	let events_loop = glutin::EventsLoop::new();
 	let builder = glutin::WindowBuilder::new().with_title("Window".to_string()).with_dimensions(800, 640).with_vsync();
 	let (window, mut device, mut factory, mut main_color, mut main_depth) = gfx_glutin::init::<ColorFormat, DepthFormat>(builder, &events_loop);
 
 	let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+	let pso = factory.create_pipeline_simple(
+		include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "./src/shaders/quad.vert")),
+		include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "./src/shaders/quad.frag")),
+		pipe::new()
+	).unwrap();
+
+	let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&SQUARE, ());
+	let mut data = pipe::Data {
+		vbuf: vertex_buffer,
+		out: main_color
+	};
+
 	let mut running = true;
+
+
 
 	while running
 	{
@@ -40,7 +74,9 @@ pub fn main()
 		});
 
 		encoder.clear(&main_color, BLACK);
+		encoder.draw(&slice, &pso, &data);
 		encoder.flush(&mut device);
+
 		window.swap_buffers().unwrap();
 		device.cleanup();
 	}
